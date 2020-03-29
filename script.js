893f3a8e01c2efdb0c6bb21abe1cf95f7c4d4f58
Kurt LaVacque <lavacquek@icloud.com>
2020-03-29 11:46:25 -0500

    Working work and break time stat calculations without hours

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ var time_div = document.querySelector(".time");
 var inputs = document.querySelector(".inputs");
 var minutesDisplay = document.querySelector("#minutes");
 var secondsDisplay = document.querySelector("#seconds");
-var totalminutesDisplay = document.querySelector("#total_minutes");
-var totalsecondsDisplay = document.querySelector("#total_seconds");
+var totalminutesDisplay = document.querySelector(".total_minutes");
+var totalsecondsDisplay = document.querySelector(".total_seconds");
+var workminutesDisplay = document.querySelector("#work_minutes");
+var worksecondsDisplay = document.querySelector("#work_seconds");
+var breakminutesDisplay = document.querySelector("#break_minutes");
+var breaksecondsDisplay = document.querySelector("#break_seconds");
 var workMinutesInput = document.querySelector("#work-minutes");
 var restMinutesInput = document.querySelector("#rest-minutes");
 var add_todo_list_item = document.querySelector(".add_button");
@@ -17,6 +21,7 @@ var reset_button_e = document.querySelector(".reset_button");
 var sessions_e = document.querySelector("#sessions");
 var total_time_e = document.querySelector("#total_time");
 var todo_list_div = document.querySelector(".todo_list_div")
+var view_stats_button_e = document.querySelector(".view_stats_button")
 var delete_button_e = document.querySelector(".delete_button")
 var audio = new Audio('./audio/Cherry.mp3');
 
@@ -35,6 +40,8 @@ var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 var totalSeconds = 0;
 var secondsElapsed = 0;
 var secondsElapsedTotal = 0;
+var secondsElapsedWork = 0;
+var secondsElapsedBreak = 0;
 var status = "Working";
 var interval;
 
@@ -101,6 +108,68 @@ function getFormattedTotalSeconds() {
   return formattedSeconds;
 }
 
+function getFormattedWorkMinutes() {
+  // var secondsLeft = totalSeconds + secondsElapsed;
+  var minutesLeftWork = Math.floor(secondsElapsedWork / 60);
+
+  var formattedMinutes;
+
+  if (minutesLeftWork < 10) {
+    formattedMinutes = "0" + minutesLeftWork;
+  } else {
+    formattedMinutes = minutesLeftWork;
+  }
+
+  return formattedMinutes;
+}
+
+function getFormattedWorkSeconds() {
+  var secondsLeftWork = (totalSeconds + secondsElapsedWork) % 60;
+
+  var formattedSeconds;
+
+  if (formattedSeconds = secondsLeftWork < 10) {
+
+    formattedSeconds = "0" + secondsLeftWork;
+  } else {
+
+    formattedSeconds = secondsLeftWork;
+  }
+
+  return formattedSeconds;
+}
+
+function getFormattedBreakMinutes() {
+  // var secondsLeft = totalSeconds + secondsElapsed;
+  var minutesLeftBreak = Math.floor(secondsElapsedBreak / 60);
+
+  var formattedMinutes;
+
+  if (minutesLeftBreak < 10) {
+    formattedMinutes = "0" + minutesLeftBreak;
+  } else {
+    formattedMinutes = minutesLeftBreak;
+  }
+
+  return formattedMinutes;
+}
+
+function getFormattedBreakSeconds() {
+  var secondsLeftBreak = (totalSeconds + secondsElapsedBreak) % 60;
+
+  var formattedSeconds;
+
+  if (formattedSeconds = secondsLeftBreak < 10) {
+
+    formattedSeconds = "0" + secondsLeftBreak;
+  } else {
+
+    formattedSeconds = secondsLeftBreak;
+  }
+
+  return formattedSeconds;
+}
+
 function setTime() {
   var minutes;
 
@@ -118,15 +187,21 @@ function setTime() {
 
 
 function renderTime() {
-  minutesDisplay.textContent = getFormattedMinutes()
+  minutesDisplay.textContent = " " + getFormattedMinutes()
   secondsDisplay.textContent = ": " + getFormattedSeconds();
+
+
   totalminutesDisplay.textContent = getFormattedTotalMinutes()
-  if (parseInt(getFormattedTotalSeconds()) < 10) {
-    totalsecondsDisplay.textContent = getFormattedTotalSeconds()
-  }
-  else {
-    totalsecondsDisplay.textContent = getFormattedTotalSeconds()
-  }
+  totalsecondsDisplay.textContent = getFormattedTotalSeconds()
+
+
+  workminutesDisplay.textContent = getFormattedWorkMinutes()
+  worksecondsDisplay.textContent = getFormattedWorkSeconds()
+
+
+  breakminutesDisplay.textContent = getFormattedBreakMinutes()
+  breaksecondsDisplay.textContent = getFormattedBreakSeconds()
+
 
 
 
@@ -172,6 +247,14 @@ function startTimer() {
   interval = setInterval(function () {
     secondsElapsed++;
     secondsElapsedTotal++;
+    if (status === "Working") {
+      secondsElapsedWork++;
+    }
+    else if (status === "Resting") {
+      secondsElapsedBreak++;
+    }
+
+
     renderTime();
   }, 1000);
 }
@@ -213,7 +296,11 @@ function toggleStatus(event) {
 
 function reset_stats() {
   session_num = 0
+  secondsLeftTotal = "00"
+  formattedMinutes = " 00"
   sessions_e.textContent = "Session: " + session_num
+  totalminutesDisplay.textContent = secondsLeftTotal
+  totalsecondsDisplay.textContent = secondsLeftTotal
 }
 
 function getTimePreferences() {
@@ -264,21 +351,42 @@ const delete_todo = (e) => {
   console.log(e.target)
 }
 
-var modal = document.getElementById("myModal");
+var time_modifier_modal = document.getElementById("time_modifier_modal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the time_modifier_modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the time_modifier_modal
+// var span = document.getElementsByClassName("close")[0];
 
 const show_time_modifiers = () => {
-  if (modal.getAttribute("style") === "display: none;") {
+  if (time_modifier_modal.getAttribute("style") === "display: none;") {
     // console.log(inputs.getAttribute("style"))
-    modal.setAttribute("style", "display: block; margin-left: 71px;")
+    time_modifier_modal.setAttribute("style", "display: block; margin-left: 71px;")
   }
   else {
     inputs.setAttribute("style", "display: none;")
+  }
+  console.log(inputs)
+}
+
+
+
+var time_modifier_modal = document.getElementById("stats_modal");
+
+// // Get the button that opens the stats_modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the stats_modal
+// var span = document.getElementsByClassName("close")[0];
+
+const show_stats = () => {
+  if (stats_modal.getAttribute("style") === "display: none;") {
+    // console.log(inputs.getAttribute("style"))
+    stats_modal.setAttribute("style", "display: block;")
+  }
+  else {
+    stats_modal.setAttribute("style", "display: none;")
   }
   console.log(inputs)
 }
@@ -302,6 +410,7 @@ document.addEventListener('click', (e) => {
 
 })
 time_div.addEventListener("click", show_time_modifiers);
+view_stats_button_e.addEventListener("click", show_stats);
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -313,3 +422,50 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
