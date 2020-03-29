3cecf08131e692e2b6c447c86e67f04135149177
Kurt LaVacque <lavacquek@icloud.com>
2020-03-29 13:13:24 -0500

    Fixed the issue with the time jsut taking whatever the total time was for the work and break time

--- script.js
+++ script.js
@@ -1,145 +1,109 @@
-var status_span = document.querySelector("#status");
-var status_toggle = document.querySelector("#status-toggle");
-var play_button = document.querySelector("#play");
-var pause_button = document.querySelector("#pause");
-var stop_button = document.querySelector("#stop");
-var time_div = document.querySelector(".time");
-var inputs = document.querySelector(".inputs");
-var minutes_display = document.querySelector("#minutes");
-var seconds_display = document.querySelector("#seconds");
-var total_hours_display = document.querySelector(".total_hours");
-var total_minutes_display = document.querySelector(".total_minutes");
-var total_seconds_display = document.querySelector(".total_seconds");
-var work_hours_display = document.querySelector("#work_hours");
-var work_minutes_display = document.querySelector("#work_minutes");
-var work_seconds_display = document.querySelector("#work_seconds");
-var break_hours_display = document.querySelector("#break_hours");
-var break_minutes_display = document.querySelector("#break_minutes");
-var break_seconds_display = document.querySelector("#break_seconds");
-var work_minutes_input = document.querySelector("#work-minutes");
-var rest_minutes_input = document.querySelector("#rest-minutes");
-var add_todo_list_item = document.querySelector(".add_button");
-var todo_input = document.querySelector(".todo_input");
-var reset_button_e = document.querySelector(".reset_button");
-var sessions_e = document.querySelector("#sessions");
-var total_time_e = document.querySelector("#total_time");
-var todo_list_div = document.querySelector(".todo_list_div")
-var view_stats_button_e = document.querySelector(".view_stats_button")
-var delete_button_e = document.querySelector(".delete_button")
-var audio = new Audio('./audio/Cherry.mp3');
+// Elements
 
-var time_stamp = Date.now();
-var date = new Date(time_stamp * 1000);
-var hours = date.getHours();
-// Minutes part from the timestamp
-var minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
-var seconds = "0" + date.getSeconds();
+// Containers
+const time_div = document.querySelector(".time");
+const todo_list_div = document.querySelector(".todo_list_div")
 
-// Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+// Time Display
+const minutes_display = document.querySelector("#minutes");
+const seconds_display = document.querySelector("#seconds");
+const total_hours_display = document.querySelector(".total_hours");
+const total_minutes_display = document.querySelector(".total_minutes");
+const total_seconds_display = document.querySelector(".total_seconds");
+const work_hours_display = document.querySelector("#work_hours");
+const work_minutes_display = document.querySelector("#work_minutes");
+const work_seconds_display = document.querySelector("#work_seconds");
+const break_hours_display = document.querySelector("#break_hours");
+const break_minutes_display = document.querySelector("#break_minutes");
+const break_seconds_display = document.querySelector("#break_seconds");
+const work_minutes_input = document.querySelector("#work-minutes");
+const rest_minutes_input = document.querySelector("#rest-minutes");
+
+// Inputs
+const inputs = document.querySelector(".inputs");
+const todo_input = document.querySelector(".todo_input");
+
+// Modals
+const stats_modal = document.getElementById("stats_modal");
+const time_modifier_modal = document.querySelector(".time_modifier_modal");
+
+// Buttons
+const play_button = document.querySelector("#play");
+const pause_button = document.querySelector("#pause");
+const stop_button = document.querySelector("#stop");
+const add_todo_list_item = document.querySelector(".add_button");
+const reset_button_e = document.querySelector(".reset_button");
+const view_stats_button_e = document.querySelector(".view_stats_button")
+const delete_button_e = document.querySelector(".delete_button")
+
+// Checkbox
+const status_toggle = document.querySelector("#status-toggle");
+
+// Stats
+const sessions_e = document.querySelector("#sessions");
+const total_time_e = document.querySelector("#total_time");
+const status_span = document.querySelector("#status");
+
+// Audo
+const audio = new Audio('./audio/Cherry.mp3');
+
+// Initializing Variables
+let total_seconds = 0;
+let seconds_elapsed = 0;
+let seconds_elapsed_total = 0;
+let seconds_elapsed_work = 0;
+let seconds_elapsed_break = 0;
+let list_item_id = 0
+let session_num = 0
+let status = "Working";
+let interval;
 
 
-var total_seconds = 0;
-var seconds_elapsed = 0;
-var seconds_elapsed_total = 0;
-var seconds_elapsed_work = 0;
-var seconds_elapsed_break = 0;
-var status = "Working";
-var interval;
-
-// getTimePreferences();
-
-
-
-
-function getFormattedMinutes() {
-  var seconds_left = total_seconds - seconds_elapsed;
-
-  var minutes_left = Math.floor(seconds_left / 60);
-
-  var formattedMinutes;
-
-  if (minutes_left < 10) {
-    formattedMinutes = "0" + minutes_left;
-  } else {
-    formattedMinutes = minutes_left;
-  }
-
-  return formattedMinutes;
+const getFormattedMinutes = () => {
+  let seconds_left = total_seconds - seconds_elapsed;
+  let minutes_left = Math.floor(seconds_left / 60);
+  return get_formatted_time(minutes_left);
 }
 
-function getFormattedSeconds() {
-  var seconds_left = (total_seconds - seconds_elapsed) % 60;
-
-  var formatted_seconds;
-
-  if (seconds_left < 10) {
-    formatted_seconds = "0" + seconds_left;
-  } else {
-    formatted_seconds = seconds_left;
-  }
-
-  return formatted_seconds;
-
+const getFormattedSeconds = () => {
+  let seconds_left = (total_seconds - seconds_elapsed) % 60;
+  return get_formatted_time(seconds_left);
 }
 
-function get_formatted_hours() {
-  // var seconds_left = total_seconds + seconds_elapsed;
-  var hours_left = Math.floor(seconds_elapsed_total / 60 / 60);
-
-  var formatted_hours;
-
-  if (hours_left < 10) {
-    formatted_hours = "0" + hours_left;
-  } else {
-    formatted_hours = hours_left;
-  }
-
-  return formatted_hours;
+const get_formatted_hours = (time_elapsed) => {
+  let hours_left = Math.floor(time_elapsed / 60 / 60);
+  return get_formatted_time(hours_left);
 }
 
-function get_formatted_minutes() {
-  // var seconds_left = total_seconds + seconds_elapsed;
-  var minutes_left = Math.floor(seconds_elapsed_total / 60);
-
-  var formatted_minutes;
-
-  if (minutes_left < 10) {
-    formatted_minutes = "0" + minutes_left;
-  } else {
-    formatted_minutes = minutes_left;
-  }
-
-  return formatted_minutes;
+const get_formatted_minutes = (time_elapsed) => {
+  let minutes_left = Math.floor(time_elapsed / 60);
+  return get_formatted_time(minutes_left);
 }
 
-function get_formatted_seconds() {
-  var seconds_left = (total_seconds + seconds_elapsed_total) % 60;
-
-  var formatted_seconds;
-
-  if (formatted_seconds = seconds_left < 10) {
-
-    formatted_seconds = "0" + seconds_left;
-  } else {
-
-    formatted_seconds = seconds_left;
-  }
-
-  return formatted_seconds;
+const get_formatted_seconds = (time_elapsed) => {
+  let seconds_left = (total_seconds + time_elapsed) % 60;
+  return get_formatted_time(seconds_left);
 }
 
 
-function setTime() {
-  var minutes;
+
+const get_formatted_time = (time_left) => {
+  let formatted_time;
+  if (formatted_time = time_left < 10) {
+    formatted_time = "0" + time_left;
+  } else {
+    formatted_time = time_left;
+  }
+  return formatted_time;
+}
+
+const setTime = () => {
+  let minutes;
 
   if (status === "Working") {
-    // minutes = work_minutes_input.value.trim();
     minutes = work_minutes_input.value.trim() || 52
   } else {
     minutes = rest_minutes_input.value.trim() || 17
-    // minutes = 17
   }
 
   clearInterval(interval);
@@ -147,65 +111,55 @@ function setTime() {
 }
 
 
-function renderTime() {
+const renderTime = () => {
   minutes_display.textContent = " " + getFormattedMinutes()
   seconds_display.textContent = ": " + getFormattedSeconds();
-
-  total_hours_display.textContent = get_formatted_hours()
-  total_minutes_display.textContent = get_formatted_minutes()
-  total_seconds_display.textContent = get_formatted_seconds()
-
+  total_hours_display.textContent = get_formatted_hours(seconds_elapsed_total)
+  total_minutes_display.textContent = get_formatted_minutes(seconds_elapsed_total)
+  total_seconds_display.textContent = get_formatted_seconds(seconds_elapsed_total)
   if (status === "Working") {
-
-    work_hours_display.textContent = get_formatted_hours()
-    work_minutes_display.textContent = get_formatted_minutes()
-    work_seconds_display.textContent = get_formatted_seconds()
+    work_hours_display.textContent = get_formatted_hours(seconds_elapsed_work)
+    work_minutes_display.textContent = get_formatted_minutes(seconds_elapsed_work)
+    work_seconds_display.textContent = get_formatted_seconds(seconds_elapsed_work)
   }
   else if (status === "Resting") {
-    break_hours_display.textContent = get_formatted_hours()
-    break_minutes_display.textContent = get_formatted_minutes()
-    break_seconds_display.textContent = get_formatted_seconds()
+    break_hours_display.textContent = get_formatted_hours(seconds_elapsed_break)
+    break_minutes_display.textContent = get_formatted_minutes(seconds_elapsed_break)
+    break_seconds_display.textContent = get_formatted_seconds(seconds_elapsed_break)
   }
-
-
-
-
   if (seconds_elapsed >= total_seconds) {
     if (status === "Working") {
       audio.play();
-      // alert("Time for a break!");
       status = "Resting";
       document.getElementById("status-toggle").checked = false;
       document.querySelector("body").setAttribute("style", "background-image: linear-gradient(0deg, rgba(173, 92, 92, 1) 9%, rgba(168, 223, 214, 1) 78%, rgba(168, 223, 214, 1) 78%)")
       add_todo_list_item.setAttribute("style", "background-color:rgba(168, 223, 214, 1);")
-      // todo_list_div.getAttribute("style", "display: none;")
-    } else {
+    }
+    else {
       audio.play();
-      // alert("Time to get back to work!");
       status = "Working";
       document.getElementById("status-toggle").checked = true;
-
       document.querySelector("body").setAttribute("style", "background-image: linear-gradient(180deg, rgba(173, 92, 92, 1) 9%, rgba(168, 223, 214, 1) 78%, rgba(168, 223, 214, 1) 78%)")
       add_todo_list_item.setAttribute("style", "background-color:rgba(173, 92, 92, 1);")
-      // todo_list_div.getAttribute("style", "display: flex;")
     }
     status_span.textContent = status;
-
     stopTimer();
   }
 }
 
-let session_num = 0
+// const render_styles = () => {
 
-function startTimer() {
+// }
+
+
+const startTimer = () => {
   if (status === "Working") {
     session_num++
-    // total_time_e
   }
   setTime();
   audio.pause();
   sessions_e.textContent = "Session: " + session_num
-  interval = setInterval(function () {
+  interval = setInterval(() => {
     seconds_elapsed++;
     seconds_elapsed_total++;
     if (status === "Working") {
@@ -214,26 +168,24 @@ function startTimer() {
     else if (status === "Resting") {
       seconds_elapsed_break++;
     }
-
-
     renderTime();
   }, 1000);
 }
 
-function pauseTimer() {
+const pauseTimer = () => {
   clearInterval(interval);
   renderTime();
   audio.pause();
 }
 
-function stopTimer() {
+const stopTimer = () => {
   seconds_elapsed = 0;
   setTime();
   renderTime();
 }
 
-function toggleStatus(event) {
-  var checked = event.target.checked;
+const toggleStatus = (event) => {
+  let checked = event.target.checked;
 
   if (checked) {
     status = "Working";
@@ -256,7 +208,7 @@ function toggleStatus(event) {
   renderTime();
 }
 
-function reset_stats() {
+const reset_stats = () => {
   seconds_elapsed = 0;
   seconds_elapsed_total = 0;
   seconds_elapsed_work = 0;
@@ -278,43 +230,15 @@ function reset_stats() {
   break_seconds_display.textContent = seconds_left
 }
 
-function getTimePreferences() {
-  var preferences = JSON.parse(localStorage.getItem("preferences"));
-
-  if (preferences) {
-    if (preferences.workMinutes) {
-      work_minutes_input.value = preferences.workMinutes;
-    }
-
-    if (preferences.restMinutes) {
-      rest_minutes_input.value = preferences.restMinutes;
-    }
-  }
-
-  setTime();
-  renderTime();
-}
-
-function setTimePreferences() {
-  localStorage.setItem(
-    "preferences",
-    JSON.stringify({
-      workMinutes: work_minutes_input.value.trim(),
-      restMinutes: rest_minutes_input.value.trim()
-    })
-  );
-}
-
-let id = 0
 const add_todo = () => {
-  id++
-  let list_item_div = document.createElement("div");
-  let list_item = document.createElement("li");
-  let delete_button = document.createElement("button");
+  list_item_id++
+  const list_item_div = document.createElement("div");
+  const list_item = document.createElement("li");
+  const delete_button = document.createElement("button");
   list_item.textContent = document.querySelector(".todo_input").value
-  delete_button.innerHTML = `<i data=${id} class='fas fa-minus-circle'></i>`
+  delete_button.innerHTML = `<i data=${list_item_id} class='fas fa-minus-circle'></i>`
   list_item_div.setAttribute("class", "list_item_div")
-  list_item_div.setAttribute("id", id)
+  list_item_div.setAttribute("id", list_item_id)
   delete_button.setAttribute("style", "background-color: unset; border: 0px; font-size: 25px; color: #ad5c5c; outline: none;")
   delete_button.setAttribute("class", "zoom delete_button")
   list_item_div.append(list_item, delete_button)
@@ -322,13 +246,7 @@ const add_todo = () => {
   document.querySelector(".todo_input").value = ""
 }
 
-const delete_todo = (e) => {
-  console.log(e.target)
-}
-
-var time_modifier_modal = document.querySelector(".time_modifier_modal");
-
-
+// Show Hide Modals
 const show_time_modifiers = () => {
   if (time_modifier_modal.getAttribute("style") === "display: none;") {
     time_modifier_modal.setAttribute("style", "display: block; margin-left: 71px;")
@@ -337,10 +255,6 @@ const show_time_modifiers = () => {
     time_modifier_modal.setAttribute("style", "display: none;")
   }
 }
-
-
-
-var stats_modal = document.getElementById("stats_modal");
 
 const show_stats = () => {
   if (stats_modal.getAttribute("style") === "display: none;") {
@@ -351,28 +265,24 @@ const show_stats = () => {
   }
 }
 
-
-
+// Event Listeners
 play_button.addEventListener("click", startTimer);
 pause_button.addEventListener("click", pauseTimer);
 stop_button.addEventListener("click", stopTimer);
 status_toggle.addEventListener("change", toggleStatus);
 add_todo_list_item.addEventListener("click", add_todo);
 reset_button_e.addEventListener("click", reset_stats);
-// delete_button_e.addEventListener("click", delete_todo);
 document.addEventListener('click', (e) => {
   const id = e.target.getAttribute('data')
-  // console.log(id)
   const list_item_to_remove = document.getElementById(id)
-  // console.log(list_item_to_remove)
   list_item_to_remove.remove()
-  // document.removeChild(list_item_to_remove);
 
 })
 time_div.addEventListener("click", show_time_modifiers);
 view_stats_button_e.addEventListener("click", show_stats);
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+
+// View Port Settings for Moble I think
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
